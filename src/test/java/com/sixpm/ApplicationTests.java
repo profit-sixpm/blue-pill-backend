11bcd2bad86aabecfd28a9844866f5e3cba0d68c
kjyy08 <kjyy08@naver.com>
2025-12-27 00:42:34 +0900

    chore: 테스트 프로파일 활성화를 위한 @ActiveProfiles 추가

--- src/test/java/com/sixpm/ApplicationTests.java
+++ src/test/java/com/sixpm/ApplicationTests.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Import(TestConfig.class)
 @EnableAutoConfiguration(exclude = {
 	io.awspring.cloud.autoconfigure.s3.S3AutoConfiguration.class,
